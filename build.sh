59c4ee4dd52b3f7b61edee541d82af307e03d7ab
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2020-03-26 20:46:58 +0100

    updated build

--- build.sh
+++ build.sh
@@ -5,13 +5,17 @@ command -v mvn >/dev/null 2>&1 || { echo >&2 "Could not call mvn, are you sure M
 
 if [ $# -eq 1 ]
 	then
-		MM2_PLUGINS_HOME=$1
+		MM2_HOME=$1
+		MM2_PLUGINS_HOME="$MM2_HOME\mmplugins"
 		
-		mvn install:install-file -Dfile="$MM2_PLUGINS_HOME\plugins\Micro-Manager\MMJ_.jar" -DgroupId=org.micromanager  -DartifactId=MMJ_ -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
-		mvn install:install-file -Dfile="$MM2_PLUGINS_HOME\plugins\Micro-Manager\MMAcqEngine.jar" -DgroupId=org.micromanager  -DartifactId=MMAcqEngine -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
-		mvn install:install-file -Dfile="$MM2_PLUGINS_HOME\plugins\Micro-Manager\MMCoreJ.jar" -DgroupId=org.micromanager  -DartifactId=MMCoreJ -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
+		mvn install:install-file -Dfile="$MM2_HOME\plugins\Micro-Manager\MMJ_.jar" -DgroupId=org.micromanager  -DartifactId=MMJ_ -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
+		mvn install:install-file -Dfile="$MM2_HOME\plugins\Micro-Manager\MMAcqEngine.jar" -DgroupId=org.micromanager  -DartifactId=MMAcqEngine -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
+		mvn install:install-file -Dfile="$MM2_HOME\plugins\Micro-Manager\MMCoreJ.jar" -DgroupId=org.micromanager  -DartifactId=MMCoreJ -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
 		
 		mvn -f accent/ clean install -Dmaven.test.skip=true
+		
+		# finally copy the jar to MM
+		cp "accent\accent-mm2\target\accent-mm2-1.0.jar" "$MM2_PLUGINS_HOME\accent-mm2-1.0.jar"
 		
 	else
 		mvn -f accent/accent-common/ clean install -Dmaven.test.skip=true
