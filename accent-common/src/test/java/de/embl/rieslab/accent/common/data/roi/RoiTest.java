d531395855dfc570b73e7b44075d06bc9c01d7e6
Joran Deschamps <jdeschamps@users.noreply.github.com>
2020-08-24 19:06:53 +0200

    fixed imports

--- accent-common/src/test/java/de/embl/rieslab/accent/common/data/roi/RoiTest.java
+++ accent-common/src/test/java/de/embl/rieslab/accent/common/data/roi/RoiTest.java
@@ -1,10 +1,12 @@
 package de.embl.rieslab.accent.common.data.roi;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 import de.embl.rieslab.accent.common.data.roi.SimpleRoi;
 
